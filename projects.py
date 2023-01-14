668aab849aad9300ffd0feac2da65dcdb23c238a
Rickard Lindberg <rickard@rickardlindberg.me>
2023-01-14 10:20:12 +0100

    Add stdin dependency.

--- projects.py
+++ projects.py
@@ -26,7 +26,13 @@ class ProjectsApp:
     ...     args=["process_email"],
     ...     stdin=Email.create_test_instance(from_address="timeline@projects.rickardlindberg.me").render()
     ... )
-    Projects!
+    From: timeline@projects.rickardlindberg.me
+    Content-Type: text/plain; charset="utf-8"
+    Content-Transfer-Encoding: 7bit
+    MIME-Version: 1.0
+    <BLANKLINE>
+    hello
+    <BLANKLINE>
 
     I fail if command is unknown:
 
@@ -46,22 +52,25 @@ class ProjectsApp:
     @staticmethod
     def create():
         return ProjectsApp(
-            args=Args.create()
+            args=Args.create(),
+            stdin=Stdin.create()
         )
 
     @staticmethod
     def run_in_test_mode(args=[], stdin=""):
         app = ProjectsApp(
-            args=Args.create_null(args)
+            args=Args.create_null(args),
+            stdin=Stdin.create_null(stdin)
         )
         return app.run()
 
-    def __init__(self, args):
+    def __init__(self, args, stdin):
         self.args = args
+        self.stdin = stdin
 
     def run(self):
         if self.args.get() == ["process_email"]:
-            print("Projects!")
+            print(self.stdin.read())
         else:
             sys.exit(f"Unknown command {self.args.get()}")
 
@@ -117,6 +126,43 @@ class Email:
 
     def set_body(self, body):
         self.email.set_content(body)
+
+class Stdin:
+
+    """
+    I am an infrastructure wrapper for reading stdin:
+
+    >>> print(subprocess.run([
+    ...     "python", "-c",
+    ...     "from projects import Stdin;"
+    ...         "print(Stdin.create().read())",
+    ... ], input="test", stdout=subprocess.PIPE, text=True).stdout.strip())
+    test
+
+    I can configure responses:
+
+    >>> Stdin.create_null("configured response").read()
+    'configured response'
+    """
+
+    @staticmethod
+    def create():
+        return Stdin(sys=sys)
+
+    @staticmethod
+    def create_null(response):
+        class NullStdin:
+            def read(self):
+                return response
+        class NullSys:
+            stdin = NullStdin()
+        return Stdin(sys=NullSys)
+
+    def __init__(self, sys):
+        self.sys = sys
+
+    def read(self):
+        return self.sys.stdin.read()
 
 class Args:
 
